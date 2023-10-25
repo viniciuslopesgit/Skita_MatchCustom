bb699c783ccb8a45cfba8ab46178958d3c838be8
Vinicius Lopes <contact.viniciuslopes@gmail.com>
2023-10-25 12:55:58 +0100

    Update upimgserver.php

--- upimgserver.php
+++ upimgserver.php
@@ -1,24 +1,16 @@
 <?php
-/*
-      header("Access-Control-Allow-Credentials", "true");
-      header("Access-Control-Allow-Headers", "Accept");
-      header("Access-Control-Allow-Methods", "POST");
-      header("Access-Control-Allow-Origin", "*");*/
-
       header("Access-Control-Allow-Credentials: true");
       header("Access-Control-Allow-Origin: *");
       header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
       header("Access-Control-Allow-Headers: Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time");
  
-      if (($_FILES["file"]["type"] == "image/png") && ($_FILES["file"]["size"] < 20000000000)){ 
+      if (($_FILES["file"]["type"] == "image/png") && ($_FILES["file"]["size"] < 20000000000))
+      { 
             if ($_FILES["file"]["error"] > 0){ 
                   echo "Return Code: " . $_FILES["file"]["error"] . ""; 
-            }else{ 
-                  // echo "Upload: " . $_FILES["file"]["name"] . ""; 
-                  // echo "Type: " . $_FILES["file"]["type"] . "";
-                  // echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb";
-                  // echo "Temp file: " . $_FILES["file"]["tmp_name"] . "";
-
+            }
+            else
+            { 
                   $nomeIni = substr($_FILES["file"]["name"],0,3);
                   $nomeIni = strtolower($nomeIni);
 
@@ -26,24 +18,28 @@
                         if (file_exists("upload/Info/" . $_FILES["file"]["name"])){
                               echo $_FILES["file"]["name"] . " already exists. ";
                         }
-                        else{
+                        else
+                        {
                               move_uploaded_file($_FILES["file"]["tmp_name"],"upload/Info/" . $_FILES["file"]["name"]);
                               echo "Storedin:" . "upload/Info/" . $_FILES["file"]["name"];
                         }
-                  }else{
-                        if (file_exists("upload/php/" . $_FILES["file"]["name"])){
+                  }
+                  else
+                  {
+                        if (file_exists("upload/php/" . $_FILES["file"]["name"]))
+                        {
                               echo $_FILES["file"]["name"] . " already exists. ";
                         }
-                        else{
+                        else
+                        {
                               move_uploaded_file($_FILES["file"]["tmp_name"], "upload/php/" . $_FILES["file"]["name"]);
                               echo "Storedin:" . "upload/php/" . $_FILES["file"]["name"];
                         }
                   }
-
-
             }
       }
-      else  { 
+      else
+      { 
             echo "Invalid file"; 
       } 
 ?>
